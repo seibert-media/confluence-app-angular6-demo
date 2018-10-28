79f6497cd341376adf12049b982ef754dbeb9ed3
mfrankl <mfrankl@seibert-media.net>
2018-10-28 17:53:27 +0100

    corrected pom

---
+++ src/main/java/net/seibertmedia/confluence/demo/action/AngularDemoAction.java
@@ -0,0 +1,13 @@
+package net.seibertmedia.confluence.demo.action;
+
+import com.atlassian.confluence.core.ConfluenceActionSupport;
+
+public class AngularDemoAction extends ConfluenceActionSupport {
+
+	private static final long serialVersionUID = -4183231125031517678L;
+
+	public String execute() {
+		return "success";
+	}
+
+}
